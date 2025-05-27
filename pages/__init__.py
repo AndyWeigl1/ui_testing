401d0a3e75c299fe444fa43df3e4286a513a42d0
AndyWeigl1 <140345538+AndyWeigl1@users.noreply.github.com>
2025-05-27 15:48:29 -0600

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -5,11 +5,13 @@ from .about_page import AboutPage
 from .process_page import ProcessPage
 from .projects_page import ProjectsPage
 from .settings_page import SettingsPage
+from .sops_page import SOPsPage
 
 __all__ = [
     'BasePage',
     'AboutPage', 
     'ProcessPage',
     'ProjectsPage',
+    'SOPsPage',
     'SettingsPage'
 ]
